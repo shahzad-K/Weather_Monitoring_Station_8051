662d1e531a9547d08f8804b439793be0cef466f7
Shahzad <72083572+kingsak@users.noreply.github.com>
2021-05-07 16:04:58 +0530

    Update temp_lcd.c

--- temp_lcd.c
+++ temp_lcd.c
@@ -1,7 +1,7 @@
 #include"temp_header.h"
 #include<reg51.h>
-#define LCD_OUT P0
 
+#define LCD_OUT P0
 sbit RS = P2^0;
 sbit RW = P2^1;
 sbit EN = P2^2;
@@ -23,16 +23,6 @@ void lcd_cmd(char_u8 cmd)
 	delay_ms(2);
 	EN = 0;
 }
-/*
-void print_ch(char_u8 ch)
-{
-	LCD_OUT = ch;
-	RS = 1;
-	RW = 0;
-	EN = 1;
-	delay_ms(2);
-	EN = 0;
-}*/
 
 void print_str(char_u8 *p)
 {
@@ -40,7 +30,7 @@ void print_str(char_u8 *p)
 	RW = 0;
 	while(*p != '\0')
 	{
-		LCD_OUT = *p;
+		LCD_OUT = *p;	//write data on LCD port
 		
 		EN = 1;
 		delay_ms(2);
@@ -48,55 +38,3 @@ void print_str(char_u8 *p)
 		++p;
 	}
 }
-/*
-char_u8 digit_count(int_u16 n)
-{
-	char_u8 i=0;
-	if(n==0)
-		return 1;
-	for( ; n; n/=10)
-		++i;
-	return i;
-}
-
-void print_int(int_u16 n)
-{
-	char_u8 i,c = digit_count(n),arr[5]={0};
-	for(i=0; i<c; ++i,n/=10)
-		arr[c-i-1] = n%10 + '0';
-	for(i=0; i<c; ++i)
-		print_ch(arr[i]);
-}
-
-void print_float(f_32 n)
-{
-	int_u32 t1=n, t2=(n-t1)*100;
-	print_int(t1);
-	print_ch('.');
-	print_int(t2);
-}
-
-
-char_u8 str_len(char_u8 *p)
-{
-	char_u8 i;
-	for(i=0; p[i] != '\0'; ++i);
-		
-	return i;	
-}
-
-void print_temperature(char_s8 t)
-{
-	if(t<0)
-	{
-		t -= 1;
-		t = ~t;
-		print_ch('-');
-	}
-	print_ch(t/100 +48);
-	print_ch(((t/10)%10) +48);	
-	print_ch(t%10 +48);
-	print_str(".C");
-	
-}
-*/
